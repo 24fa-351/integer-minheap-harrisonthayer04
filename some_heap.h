669fa310119d733b5abbc2c4f2f4e59e20ae8ed4
harrisonthayer <harrisonthayer04@gmail.com>
2024-09-30 16:59:30 -0700

    Finished the assignment

--- some_heap.h
+++ some_heap.h
@@ -25,6 +25,20 @@ void heap_free(heap_t *heap);
 
 unsigned int heap_size(heap_t *heap);
 
+unsigned int heap_parent(unsigned int index);
+
+unsigned int heap_left_child(unsigned int index);
+
+unsigned int heap_right_child(unsigned int index);
+
+unsigned int heap_level(unsigned int index);
+
+void heap_swap(heap_t *heap, int index1, int index2);
+
+void heap_bubble_up(heap_t *heap, int index);
+
+void heap_bubble_down(heap_t *heap, int index);
+
 void heap_insert(heap_t *heap, heap_key_t key, heap_value_t data);
 
 heap_value_t heap_remove_min(heap_t *heap);
